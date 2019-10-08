2e37e9667d586030d16db69a80ccaef04484839e
ksenya-torkova <ksenya.torkova@gmail.com>
2019-10-08 22:58:05 +0300

    выполняет задание "В движении", оживляет перетаскивание диалогового окна

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -7,10 +7,53 @@
     evt.preventDefault();
 
     var startCoordinates = {
-      setupHandlerX: evt.clientX,
-      setupHandlerY: evt.clientY
+      x: evt.clientX,
+      y: evt.clientY
     };
 
+    var isDragged = false;
 
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      isDragged = true;
+
+      var shift = {
+        x: startCoordinates.x - moveEvt.clientX,
+        y: startCoordinates.y - moveEvt.clientY
+      };
+
+      startCoordinates = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.setup.style.top = (window.setup.offsetTop - shift.y) + 'px';
+      window.setup.style.left = (window.setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      startCoordinates = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (isDragged) {
+        var onUploadClick = function (fileEvt) {
+          fileEvt.preventDefault();
+
+          setupHandler.removeEventListener('click', onUploadClick);
+        };
+
+        setupHandler.addEventListener('click', onUploadClick);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
